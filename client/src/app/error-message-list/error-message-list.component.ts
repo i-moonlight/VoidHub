d5dbe7644ec03afe99c05d04e8caaa3ee5c187ba
i-moonlight <infinite.moonlight0506@gmail.com>
2023-09-20 22:18:35 +0300

    add error-list

---
+++ client/src/app/error-message-list/error-message-list.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-error-message-list',
+  templateUrl: './error-message-list.component.html',
+  styleUrls: ['./error-message-list.component.css'],
+  standalone: true,
+  imports: [
+    CommonModule
+  ]
+})
+export class ErrorMessageListComponent {
+  @Input("messages")
+  errorMessages: string[] = [];
+}
