a8de22046ee6f5758c38cdb5fea88d04301f14ba
i-moonlight <infinite.moonlight0506@gmail.com>
2023-11-01 10:15:00 +0300

    upd forbidden exception

--- API/ForumApi/Exceptions/ForbiddenException.cs
+++ API/ForumApi/Exceptions/ForbiddenException.cs
@@ -1,4 +1,6 @@
 
+using Newtonsoft.Json;
+
 namespace ForumApi.Exceptions
 {
     public class ForbiddenException : BaseException
@@ -10,5 +12,9 @@ namespace ForumApi.Exceptions
         public ForbiddenException(string message, Exception inner) : base(message, inner)
         {
         }
+
+        public ForbiddenException(object errorObj) : base(JsonConvert.SerializeObject(errorObj)) {}
+
+        public ForbiddenException(object errorObj, Exception inner) : base(JsonConvert.SerializeObject(errorObj), inner) {}
     }
 }
