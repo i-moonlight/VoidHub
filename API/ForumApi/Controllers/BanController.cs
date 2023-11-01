11d4636e385bdaa3d21b07035b680b8198234a92
i-moonlight <infinite.moonlight0506@gmail.com>
2023-11-01 10:16:40 +0300

    add ban controller

---
+++ API/ForumApi/Controllers/BanController.cs
@@ -0,0 +1,54 @@
+using ForumApi.Data.Models;
+using ForumApi.DTO.DBan;
+using ForumApi.DTO.Page;
+using ForumApi.Extensions;
+using ForumApi.Filters;
+using ForumApi.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForumApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/bans")]
+    public class BanController : ControllerBase
+    {
+        private readonly IBanService _banService;
+
+        public BanController(IBanService banService)
+        {
+            _banService = banService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBans([FromQuery] Page page)
+        {
+            return Ok(await _banService.GetBans(page));
+        }
+
+        [HttpPost]
+        [Authorize(Roles = $"{Role.Admin},{Role.Moder}")]
+        [BanFilter]
+        public async Task<IActionResult> Create(BanDto ban)
+        {
+            return Ok(await _banService.Create(User.GetId(), ban));
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(Roles = $"{Role.Admin},{Role.Moder}")]
+        [BanFilter]
+        public async Task<IActionResult> Update(int id, BanDto ban)
+        {
+            return Ok(await _banService.Update(User.GetId(), id, ban));
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = $"{Role.Admin},{Role.Moder}")]
+        [BanFilter]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _banService.Delete(User.GetId(), id);
+            return Ok();
+        }
+    }
+}
