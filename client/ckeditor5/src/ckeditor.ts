e1c0e908b28f98992376b6c781f3416ddbec4cbd
i-moonlight <infinite.moonlight0506@gmail.com>
2023-11-13 12:17:04 +0300

    upd ckeditor project

--- client/ckeditor5/src/ckeditor.ts
+++ client/ckeditor5/src/ckeditor.ts
@@ -7,29 +7,29 @@ import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
 
 import { Alignment } from '@ckeditor/ckeditor5-alignment';
 import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
-import { Bold, Code, Italic, Underline } from '@ckeditor/ckeditor5-basic-styles';
+import { Bold, Italic, Underline } from '@ckeditor/ckeditor5-basic-styles';
 import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
 import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
 import { Essentials } from '@ckeditor/ckeditor5-essentials';
 import { FontBackgroundColor, FontColor, FontFamily, FontSize } from '@ckeditor/ckeditor5-font';
-import { Heading } from '@ckeditor/ckeditor5-heading';
 import { Highlight } from '@ckeditor/ckeditor5-highlight';
 import {
+	AutoImage,
 	Image,
 	ImageCaption,
 	ImageResize,
 	ImageStyle,
 	ImageToolbar
 } from '@ckeditor/ckeditor5-image';
+import { Indent, IndentBlock } from '@ckeditor/ckeditor5-indent';
 import { AutoLink, Link } from '@ckeditor/ckeditor5-link';
 import { List } from '@ckeditor/ckeditor5-list';
-import { MediaEmbed, MediaEmbedToolbar } from '@ckeditor/ckeditor5-media-embed';
+import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
 import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
 import { RemoveFormat } from '@ckeditor/ckeditor5-remove-format';
 import { SelectAll } from '@ckeditor/ckeditor5-select-all';
 import {
 	Table,
-	TableCaption,
 	TableCellProperties,
 	TableColumnResize,
 	TableProperties,
@@ -42,34 +42,33 @@ import {
 class Editor extends ClassicEditor {
 	public static override builtinPlugins = [
 		Alignment,
+		AutoImage,
 		AutoLink,
 		Autoformat,
 		BlockQuote,
 		Bold,
-		Code,
 		CodeBlock,
 		Essentials,
 		FontBackgroundColor,
 		FontColor,
 		FontFamily,
 		FontSize,
-		Heading,
 		Highlight,
 		Image,
 		ImageCaption,
 		ImageResize,
 		ImageStyle,
 		ImageToolbar,
+		Indent,
+		IndentBlock,
 		Italic,
 		Link,
 		List,
 		MediaEmbed,
-		MediaEmbedToolbar,
 		Paragraph,
 		RemoveFormat,
 		SelectAll,
 		Table,
-		TableCaption,
 		TableCellProperties,
 		TableColumnResize,
 		TableProperties,
@@ -80,8 +79,6 @@ class Editor extends ClassicEditor {
 	public static override defaultConfig = {
 		toolbar: {
 			items: [
-				'heading',
-				'|',
 				'bold',
 				'italic',
 				'underline',
@@ -92,18 +89,17 @@ class Editor extends ClassicEditor {
 				'fontColor',
 				'fontBackgroundColor',
 				'alignment',
+				'|',
 				'bulletedList',
 				'numberedList',
 				'|',
-				'selectAll',
 				'removeFormat',
 				'-',
-				'codeBlock',
-				'code',
 				'link',
+				'codeBlock',
+				'blockQuote',
 				'mediaEmbed',
-				'insertTable',
-				'blockQuote'
+				'insertTable'
 			],
 			shouldNotGroupWhenFull: true
 		},
@@ -125,7 +121,35 @@ class Editor extends ClassicEditor {
 				'tableCellProperties',
 				'tableProperties'
 			]
-		}
+		},
+    mediaEmbed: {
+      // git with default providers config:
+      // https://github.com/ckeditor/ckeditor5/blob/master/packages/ckeditor5-media-embed/src/mediaembedediting.ts
+      // cd .\ckeditor5\ && npm run build && cd .. && npm install ./ckeditor5/ && ng serve
+      providers: [
+        {
+          name: "youtube-nocookie",
+          url: [
+						/^(?:m\.)?youtube\.com\/watch\?v=([\w-]+)(?:&t=(\d+))?/,
+						/^(?:m\.)?youtube\.com\/v\/([\w-]+)(?:\?t=(\d+))?/,
+						/^youtube\.com\/embed\/([\w-]+)(?:\?start=(\d+))?/,
+						/^youtu\.be\/([\w-]+)(?:\?t=(\d+))?/
+					],
+          html: (match:any) => {
+            const id = match[ 1 ];
+            const time = match[ 2 ];
+
+            return (
+              '<div>' +
+                `<iframe src="https://www.youtube-nocookie.com/embed/${ id }${ time ? `?start=${ time }` : '' }" ` +
+                  'frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>' +
+                '</iframe>' +
+              '</div>'
+            );
+          }
+        },
+      ],
+    }
 	};
 }
 
